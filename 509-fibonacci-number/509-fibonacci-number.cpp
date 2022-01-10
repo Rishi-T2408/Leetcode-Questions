0f937d893c29a310f660443d4da2824ee9248ec1
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-01-10 10:51:21 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.82%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
 
     int fib(int n) {
-        if(n==0 || n==1)
+        int t[31]={-1};
+        t[0]=0;
+        t[1]=1;
+        for(int i=2;i<=n;i++)
         {
-            return n;
+            t[i]=t[i-1]+t[i-2];
         }
-        return fib(n-1)+fib(n-2);
+        return t[n];
     }
 };

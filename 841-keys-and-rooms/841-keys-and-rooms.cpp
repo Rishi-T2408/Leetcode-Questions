2fb73313013486ce2ae5d61ff16634c662c00657
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-01-22 12:45:55 +0530

    Time: 14 ms (35.49%), Space: 10.5 MB (38.19%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>>& rooms,vector<int> &vis,int curr)
+    void BFS(vector<vector<int>>& rooms,vector<int> &vis,int curr)
     {
-        vis[curr]=1;
-        for(int i=0;i<rooms[curr].size();i++)
+        queue<int> q;
+        q.push(curr);
+        int node;
+        while(!q.empty())
         {
-            int child=rooms[curr][i];
-            if(vis[child]==0)
+            node=q.front();
+            q.pop();
+            vis[node]=1;
+            for(int i=0;i<rooms[node].size();i++)
             {
-                DFS(rooms,vis,child);
+                int child=rooms[node][i];
+                if(vis[child]==0)
+                {
+                    q.push(child);
+                }
             }
         }
     }
     
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         vector<int> vis(rooms.size(),0);
-        DFS(rooms,vis,0);
+        BFS(rooms,vis,0);
         for(int i=0;i<rooms.size();i++)      //We will traverse threw graph and if any node is still unvisited then we cannot unlock it
         {
             if(vis[i]==0)

2afadc169b25251ce38d11ae9cd3719207aaccb6
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-01-30 11:20:06 +0530

    Time: 377 ms (27.66%), Space: 10.4 MB (62.97%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -8,7 +8,7 @@ public:
         {
             dp[i]=1;   
         }
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<i;j++)   
             {
@@ -17,11 +17,9 @@ public:
                     dp[i]=max(dp[i],dp[j]+1);
                 }
             }
-        }
-        for(int i=0;i<n;i++)
-        {
             ans=max(ans,dp[i]);
         }
+        
         return ans;
     }
     

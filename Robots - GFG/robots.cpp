b9600bbbef1fdcd437d1f42c8505ca35afb7c6f3
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-02-25 11:53:37 +0530

    Added solution - LeetHub

--- Robots - GFG/robots.cpp
+++ Robots - GFG/robots.cpp
@@ -10,34 +10,34 @@ using namespace std;
 class Solution{   
 public:
     string moveRobots(string s1, string s2){
-         stack <pair <char,int>> st;
-        int n2=s2.size();
-        for(int i=n2-1;i>=0;i--){
-            
-            if(s2[i]!='#'){
-                st.push({s2[i],i});
-            }
-            
+        if(s1.length()!=s2.length())
+        {
+            return "No";
         }
-        int n1=s1.size();
-        for(int i=0;i<n1;i++){
-            if(s1[i]!='#'){
-                if(s1[i]==st.top().first){
-                    if(s1[i]=='A' && st.top().second<=i){
-                        st.pop();
-                    }
-                    if(s1[i]=='B' && st.top().second>=i){
-                        st.pop();
-                    }
-                }
-                else{
-                    return "No";
-                }
+        stack<pair<char,int>> st;
+        int n2=s2.length();
+        for(int i=(n2-1);i>=0;i--)
+        {
+            if(s2[i]!='#')
+            st.push({s2[i],i});
+        }
+        for(int i=0;i<s1.length();i++)
+        {
+            //Jbhh stack ktmm hoojayega tohh top vaala fuction kaam hi nhi krega iliye lafda patti hoojayega
+            if(!st.empty() && s1[i]==st.top().first)
+            {
+                if(s1[i]=='B' && st.top().second>=i)       //B is moving right or staying in it's current position only
+                {
+                    st.pop();
+                }   
+                if(s1[i]=='A' && st.top().second<=i)  //Means A in it's final state is moving left or staying in it's same place so this is also correnct
+                {
+                    st.pop();
+                }    
             }
         }
-        if(st.empty())
+        if(st.size()==0)
         return "Yes";
-        
         return "No";
     }
 };

667a2a84a31141c15f5e213dea0da8c86cb14009
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-03-10 11:11:17 +0530

    Added solution - LeetHub

--- 7 Segment Display - GFG/7-segment-display.cpp
+++ 7 Segment Display - GFG/7-segment-display.cpp
@@ -6,20 +6,24 @@ using namespace std;
 class Solution {
   public:
     string sevenSegments(string S, int N) {
-        S=S.substr(0,N);
-        int segment[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 5};
-        int cnt = 0;
-        for(auto i:S){
-            cnt += segment[i-'0'];
+        S=S.substr(0,N);  //String length N se zyada hooskti hai aur hmme N ke barabrr hi chahiye
+        int segment[]={6,2,5,5,4,5,6,3,7,5};
+        int cnt=0;
+        for(int i=0;i<N;i++)
+        {
+            cnt+=segment[S[i]-'0'];
         }
-        string x = "" ;
-        for(int i = N-1; i>=0;i--){
-            for(int j=0; j<10; j++){
-               if(cnt-segment[j]>=2*i && cnt-segment[j]<=7*i){
-                   x += (j+'0');
-                   cnt -= segment[j];
-                   break;
-               }
+        string x="";
+        for(int i=(N-1);i>=0;i--)
+        {
+            for(int j=0;j<=9;j++)
+            {
+                if((cnt-segment[j])>=2*i && (cnt-segment[j])<=7*i)   //segment table mai number 2and 7 ki range mai hi hai
+                {
+                    x+=j+'0';
+                    cnt-=segment[j];
+                    break;
+                }
             }
         }
         return x;

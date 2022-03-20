7463e9a2fd3733dc1ebb492f15ce53c37f7db612
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-03-20 12:14:50 +0530

    Added solution - LeetHub

--- Choose and Swap - GFG/choose-and-swap.cpp
+++ Choose and Swap - GFG/choose-and-swap.cpp
@@ -23,19 +23,25 @@ public:
     }
 
     string chooseandswap(string a){
-        vector<int> first(26, 100001);
-        for(int i=a.length()-1;i>=0;i--)
-            first[a[i] -'a'] = i;
-    
+        vector<int> first(26,100001);
+        int n=a.length();
+        for(int i=(n-1);i>=0;i--)
+        {
+            first[a[i]-'a']=i;
+        }
         for(int i=0;i<26;i++)
         {
-            if(first[i]<100001){
-                int minn = 100001;
-                for(int j=i+1;j<26;j++)
-                    minn = min(minn, first[j]);
-    
-                if(minn<first[i])
-                    return Swap(a, 'a'+i, a[minn]);
+            if(first[i]<100001)  //Maane this character is coming in the string
+            {
+                int mn=100001;
+                for(int j=(i+1);j<26;j++)
+                {
+                    mn=min(mn,first[j]);            //Badha charater choote character se konse min index pe aarha hai yehh tohh hmme isse pta chll jaayega
+                }
+                if(mn<first[i])
+                {
+                    return Swap(a,i+'a',a[mn]);
+                }
             }
         }
         return a;

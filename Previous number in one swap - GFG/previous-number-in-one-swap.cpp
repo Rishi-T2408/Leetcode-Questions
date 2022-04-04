abb59611132d649e7895d5bd950101fc24d3ef6f
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-04-04 13:19:39 +0530

    Added solution - LeetHub

--- Previous number in one swap - GFG/previous-number-in-one-swap.cpp
+++ Previous number in one swap - GFG/previous-number-in-one-swap.cpp
@@ -6,28 +6,31 @@ using namespace std;
 class Solution{   
 public:
     string previousNumber(string S){
-          int n = S.length();
-       int ind = n-1;
-       while(ind > 0){
-           if(S[ind-1] > S[ind]) break;
-           ind--;
-       }
-       
-       if(ind == 0){
-           return "-1";
-       }
-       
-       int i = n-1, p = ind;
-       char curr = (char)47;
-       while(i >= ind){
-           if(S[i] < S[ind-1] && S[i] >= curr) p = i, curr = S[i];
-           i--;
-       }
-       
-       swap(S[ind-1],S[p]);
-       if(S[0] == '0') return ("-1");
-       
-       return (S);
+        int n=S.length();
+        int ind=(n-1);
+        while(ind>0)
+        {
+            if(S[ind-1]>S[ind])
+            {
+                break;
+            }
+            ind--;
+        }
+        if(ind==0) return "-1";
+        int i=(n-1),p=ind;
+        char curr=(char)47;
+        while(i>=ind)
+        {
+            if(S[ind-1]>S[i] && S[i]>=curr)
+            {
+                curr=S[i];
+                p=i;
+            }
+            i--;
+        }
+        swap(S[ind-1],S[p]);
+        if(S[0]=='0') return "-1";
+        return S;
     }
 };
 

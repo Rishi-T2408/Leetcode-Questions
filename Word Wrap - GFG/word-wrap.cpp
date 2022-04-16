7009193368aa8f21fd455913d95e2ba1babdcfe5
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-04-16 21:29:43 +0530

    Added solution - LeetHub

--- Word Wrap - GFG/word-wrap.cpp
+++ Word Wrap - GFG/word-wrap.cpp
@@ -11,35 +11,33 @@ class Solution {
 public:
     int dp[505];
     
-    int f(vector<int> nums , int idx ,int spc){
-        if(idx >= nums.size()) return 0;
-        int org = spc;
-        int res = INT_MAX;
-        int ans = 0;
-        if(dp[idx] != -1) return dp[idx];
-        for(int i=idx; i<nums.size()  && spc > nums[i] ; i++ ){
+    
+    int f(vector<int> &nums,int idx,int spc)
+    {
+        if(idx>=nums.size()) return 0;
+        int org=spc;
+        int res=INT_MAX;
+        int ans=0;
+        if(dp[idx]!=-1) return dp[idx];
+        for(int i=idx; i<nums.size() && spc>nums[i] ;i++)
+        {
+            spc-=(nums[i]+1);
+            ans=spc*spc;
+            int k=i==nums.size()-1?0:ans+f(nums,i+1,org);
             
-            spc-=(nums[i] + 1);
-           
-            ans = (spc*spc);
-            
-            
-            int k = i == nums.size() - 1 ? 0 : ans + f(nums,  i+ 1 , org) ;
-            
-            res = min(res , k);
-            
-           
+            res=min(res,k);
         }
-        return dp[idx] = res;
+        
+        return dp[idx]=res;
     }
     
     
     int solveWordWrap(vector<int>nums, int k) 
     { 
         k++;
-        int spc = k;
-        memset(dp, -1 , sizeof(dp));
-        int ans = f(nums , 0 , k);
+        int spc=k;
+        memset(dp,-1,sizeof(dp));
+        int ans=f(nums,0,k);
         return dp[0];
     } 
 };

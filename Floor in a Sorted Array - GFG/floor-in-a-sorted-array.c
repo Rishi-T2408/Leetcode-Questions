6a8fb8ebe0308b11503ee1e30b2cbbef4497df0a
Rishi Raj Tiwari <69381364+Rishi-T2408@users.noreply.github.com>
2022-06-06 23:32:50 +0530

    Added solution - LeetHub

---
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.c
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+// Function to find floor of K
+// arr[]: integer array of size N
+// N: size of arr[]
+// K: element whose floor is to find
+int findFloor(long long int arr[], int N, long long int x) {
+    //Your code here
+    long long int l=0;
+    long long int r=(N-1);
+    while(l<=r)
+    {
+        long long int mid=l+(r-l)/2;
+        if(arr[mid]==x) return mid;
+        if(arr[mid]>x) r=mid-1;
+        else l=mid+1;
+    }
+    return r;
+}
+
+// { Driver Code Starts.
+
+int main() {
+	
+	long long int t;
+	scanf("%lld", &t);
+	
+	while(t--){
+	    int n;
+	    scanf("%d", &n);
+	    long long int x;
+	    scanf("%lld", &x);
+	    
+	    long long int *arr;
+		arr = (long long int *)malloc(n * sizeof(long long int));
+	    
+	    for(int i = 0;i<n;i++){
+	        scanf("%lld", &arr[i]);
+	    }
+	    printf("%d\n", findFloor(arr, n, x) );
+	   
+	}
+	
+	return 0;
+}
+
+  // } Driver Code Ends
